ffd67b32af1604c1c4d1918435fe862635f02d7a
nxsofsys <nxsofsys@gmail.com>
2017-10-27 15:31:12 +0300

    snappy errors

--- shared/common/boundary_model.py
+++ shared/common/boundary_model.py
@@ -560,8 +560,6 @@ class BoundaryApp:
                     random.uniform(0.6, 1.0), random.uniform(0.6, 1.0)]
                 v.opacity=0.8
                 v.color = color
-                for m in v.get_mappers():
-                    m.SelectColorArray("")
             else:
                 v.visible=False
 
